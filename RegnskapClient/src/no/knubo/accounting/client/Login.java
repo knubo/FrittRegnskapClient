bd8029a329487dbcb51352480de1e378c39072b7
knutbo <knutbo@e78aed00-ab2d-0410-9d8d-9fd7b96180e8>
2007-04-10 13:38:07 +0000

    More login functionallity with forward to page.
    git-svn-id: https://accountclient.googlecode.com/svn/trunk@7 e78aed00-ab2d-0410-9d8d-9fd7b96180e8

--- RegnskapClient/src/no/knubo/accounting/client/Login.java
+++ RegnskapClient/src/no/knubo/accounting/client/Login.java
@@ -2,6 +2,10 @@ package no.knubo.accounting.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.json.client.JSONObject;
+import com.google.gwt.json.client.JSONParser;
+import com.google.gwt.json.client.JSONString;
+import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.user.client.HTTPRequest;
 import com.google.gwt.user.client.ResponseTextHandler;
 import com.google.gwt.user.client.ui.Button;
@@ -18,7 +22,6 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class Login implements EntryPoint, ClickListener, ResponseTextHandler {
 
-
 	private PasswordTextBox passBox;
 
 	private Constants constants;
@@ -33,7 +36,7 @@ public class Login implements EntryPoint, ClickListener, ResponseTextHandler {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
-	    messages = (I18NAccount) GWT.create(I18NAccount.class);
+		messages = (I18NAccount) GWT.create(I18NAccount.class);
 		constants = (Constants) GWT.create(Constants.class);
 
 		Grid grid = new Grid(4, 2);
@@ -44,7 +47,7 @@ public class Login implements EntryPoint, ClickListener, ResponseTextHandler {
 		userBox = new TextBox();
 		passBox = new PasswordTextBox();
 		infoLabel = new HTML();
-		
+
 		grid.setText(0, 0, messages.user());
 		grid.setWidget(0, 1, userBox);
 		grid.setText(1, 0, messages.password());
@@ -57,7 +60,7 @@ public class Login implements EntryPoint, ClickListener, ResponseTextHandler {
 	public void onClick(Widget sender) {
 		String user = this.userBox.getText();
 		String password = this.passBox.getText();
-		if(!HTTPRequest.asyncGet(this.constants.baseurl()
+		if (!HTTPRequest.asyncGet(this.constants.baseurl()
 				+ "authenticate.php?user=" + user + "&password=" + password,
 				this)) {
 			infoLabel.setText(messages.failedLogin());
@@ -65,6 +68,21 @@ public class Login implements EntryPoint, ClickListener, ResponseTextHandler {
 	}
 
 	public void onCompletion(String responseText) {
-		infoLabel.setHTML(responseText);
+		JSONValue jsonValue = JSONParser.parse(responseText);
+
+		JSONObject isObject = jsonValue.isObject();
+
+		JSONValue error = isObject.get("error");
+
+		if(error != null) {
+			JSONString string = error.isString();
+			infoLabel.setText(string.stringValue());
+		} else {
+			JSONValue url = isObject.get("url");
+			JSONString string = url.isString();
+
+			Util.forward(string.stringValue());
+		}
 	}
+
 }
