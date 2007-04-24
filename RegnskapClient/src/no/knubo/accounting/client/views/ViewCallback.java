57d895b6ea5836e4357dc3d597eb75bff8dee96a
knutbo <knutbo@e78aed00-ab2d-0410-9d8d-9fd7b96180e8>
2007-04-24 18:02:51 +0000

    ...
    git-svn-id: https://accountclient.googlecode.com/svn/trunk@41 e78aed00-ab2d-0410-9d8d-9fd7b96180e8

--- RegnskapClient/src/no/knubo/accounting/client/views/ViewCallback.java
+++ RegnskapClient/src/no/knubo/accounting/client/views/ViewCallback.java
@@ -2,6 +2,16 @@ package no.knubo.accounting.client.views;
 
 public interface ViewCallback {
 
-	/** Requests that detail vindow is opened. */
+	/** Requests that detail vindow is opened. 
+	 * 
+	 * @param id
+	 */
 	public void openDetails(String id);
+
+	/** Request view of the given month and year. 
+	 * 
+	 * @param year
+	 * @param month
+	 */
+	public void viewMonth(String year, String month);
 }
