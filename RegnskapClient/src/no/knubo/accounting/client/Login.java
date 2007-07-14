cd2b56d9985cf9f0e3daabf0634470ff49003442
knutbo <knutbo@e78aed00-ab2d-0410-9d8d-9fd7b96180e8>
2007-07-14 22:01:57 +0000

    Better styleing for login.
    git-svn-id: https://accountclient.googlecode.com/svn/trunk@139 e78aed00-ab2d-0410-9d8d-9fd7b96180e8

--- RegnskapClient/src/no/knubo/accounting/client/Login.java
+++ RegnskapClient/src/no/knubo/accounting/client/Login.java
@@ -2,16 +2,17 @@ package no.knubo.accounting.client;
 
 import com.google.gwt.core.client.EntryPoint;
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.http.client.Response;
 import com.google.gwt.json.client.JSONObject;
 import com.google.gwt.json.client.JSONParser;
 import com.google.gwt.json.client.JSONString;
 import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.user.client.HTTPRequest;
 import com.google.gwt.user.client.ResponseTextHandler;
+import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.ClickListener;
-import com.google.gwt.user.client.ui.Grid;
+import com.google.gwt.user.client.ui.DockPanel;
+import com.google.gwt.user.client.ui.FlexTable;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.PasswordTextBox;
 import com.google.gwt.user.client.ui.RootPanel;
@@ -23,67 +24,79 @@ import com.google.gwt.user.client.ui.Widget;
  */
 public class Login implements EntryPoint, ClickListener, ResponseTextHandler {
 
-	private PasswordTextBox passBox;
+    private PasswordTextBox passBox;
 
-	private Constants constants;
+    private Constants constants;
 
-	private HTML infoLabel;
+    private HTML infoLabel;
 
-	private I18NAccount messages;
+    private I18NAccount messages;
 
-	private TextBox userBox;
+    private TextBox userBox;
 
-	/**
-	 * This is the entry point method.
-	 */
-	public void onModuleLoad() {
-		messages = (I18NAccount) GWT.create(I18NAccount.class);
-		constants = (Constants) GWT.create(Constants.class);
+    /**
+     * This is the entry point method.
+     */
+    public void onModuleLoad() {
+        messages = (I18NAccount) GWT.create(I18NAccount.class);
+        constants = (Constants) GWT.create(Constants.class);
 
-		Grid grid = new Grid(4, 2);
+        DockPanel dp = new DockPanel();
+        dp.setStyleName("middle");
+        FlexTable table = new FlexTable();
+        table.setStyleName("edittable");
 
-		Button loginButton = new Button(messages.login());
-		loginButton.addClickListener(this);
+        dp.add(table, DockPanel.CENTER);
 
-		userBox = new TextBox();
-		passBox = new PasswordTextBox();
-		infoLabel = new HTML();
+        Button loginButton = new Button(messages.login());
+        loginButton.addClickListener(this);
 
-		grid.setText(0, 0, messages.user());
-		grid.setWidget(0, 1, userBox);
-		grid.setText(1, 0, messages.password());
-		grid.setWidget(1, 1, passBox);
-		grid.setWidget(2, 1, loginButton);
-		grid.setWidget(3, 1, infoLabel);
-		RootPanel.get().add(grid);
-	}
+        userBox = new TextBox();
+        userBox.setVisibleLength(14);
+        userBox.setMaxLength(12);
+        passBox = new PasswordTextBox();
+        passBox.setWidth("12em");
+        infoLabel = new HTML();
 
-	public void onClick(Widget sender) {
-		String user = this.userBox.getText();
-		String password = this.passBox.getText();
-		if (!HTTPRequest.asyncGet(this.constants.baseurl()
-				+ "authenticate.php?user=" + user + "&password=" + password,
-				this)) {
-			infoLabel.setText(messages.failedConnect());
-		}
-	}
+        table.setText(0, 0, messages.login());
+        table.getFlexCellFormatter().setColSpan(0, 0, 2);
+        table.setText(1, 0, messages.user());
+        table.setWidget(1, 1, userBox);
+        table.setText(2, 0, messages.password());
+        table.setWidget(2, 1, passBox);
+        table.setWidget(3, 1, loginButton);
+        table.setWidget(4, 1, infoLabel);
+        table.getFlexCellFormatter().setColSpan(4, 1, 2);
+        RootPanel.get().add(dp);
+        Window.setTitle(messages.login());
+    }
 
-	public void onCompletion(String responseText) {
-		JSONValue jsonValue = JSONParser.parse(responseText);
+    public void onClick(Widget sender) {
+        String user = this.userBox.getText();
+        String password = this.passBox.getText();
+        if (!HTTPRequest.asyncGet(this.constants.baseurl()
+                + "authenticate.php?user=" + user + "&password=" + password,
+                this)) {
+            infoLabel.setText(messages.failedConnect());
+        }
+    }
 
-		JSONObject isObject = jsonValue.isObject();
+    public void onCompletion(String responseText) {
+        JSONValue jsonValue = JSONParser.parse(responseText);
 
-		JSONValue error = isObject.get("error");
+        JSONObject isObject = jsonValue.isObject();
 
-		if(error != null) {
-			JSONString string = error.isString();
-			infoLabel.setText(string.stringValue());
-		} else {
-			JSONValue url = isObject.get("url");
-			JSONString string = url.isString();
+        JSONValue error = isObject.get("error");
 
-			Util.forward(string.stringValue());
-		}
-	}
+        if (error != null) {
+            JSONString string = error.isString();
+            infoLabel.setText(string.stringValue());
+        } else {
+            JSONValue url = isObject.get("url");
+            JSONString string = url.isString();
+
+            Util.forward(string.stringValue());
+        }
+    }
 
 }
