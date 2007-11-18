c6e6cf789d152f2627b83984a4e0ed942e6186bc
knutbo <knutbo@e78aed00-ab2d-0410-9d8d-9fd7b96180e8>
2007-11-18 10:40:46 +0000

    Bumped version number.
    git-svn-id: https://accountclient.googlecode.com/svn/trunk@241 e78aed00-ab2d-0410-9d8d-9fd7b96180e8

--- RegnskapClient/src/no/knubo/accounting/client/views/AboutView.java
+++ RegnskapClient/src/no/knubo/accounting/client/views/AboutView.java
@@ -17,7 +17,7 @@ import com.google.gwt.user.client.ui.Frame;
 public class AboutView extends Composite {
 
     /** This must match Version.php's version */
-    public static final String CLIENT_VERSION = "1.2B1";
+    public static final String CLIENT_VERSION = "1.2B2";
 
     private static AboutView instance;
 
