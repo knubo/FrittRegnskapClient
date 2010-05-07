db112049e18f45b5e56fc9b849163d98d44bcc87
knutbo <knutbo@e78aed00-ab2d-0410-9d8d-9fd7b96180e8>
2010-05-07 16:09:41 +0000

    Help fix
    git-svn-id: https://accountclient.googlecode.com/svn/trunk@329 e78aed00-ab2d-0410-9d8d-9fd7b96180e8

--- RegnskapClient/src/no/knubo/accounting/client/help/HelpPanel.java
+++ RegnskapClient/src/no/knubo/accounting/client/help/HelpPanel.java
@@ -23,133 +23,131 @@ import com.google.gwt.user.client.ui.Widget;
 
 public class HelpPanel extends Composite implements NativePreviewHandler {
 
-	DisclosurePanel disclosurePanel;
-	private static HelpPanel me;
+    DisclosurePanel disclosurePanel;
+    private static HelpPanel me;
 
-	private Frame mainFrame;
-	private HTML contextHelp;
-	private final Elements messages;
-	private final HelpTexts helpTexts;
-	private String help;
-	private String prevHelp = "";
-	private HandlerRegistration nativePreviewHandler;
+    private Frame mainFrame;
+    private HTML contextHelp;
+    private final Elements messages;
+    private final HelpTexts helpTexts;
+    private String help;
+    private String prevHelp = "";
+    private HandlerRegistration nativePreviewHandler;
 
-	public void setCurrentWidget(Widget widget, WidgetIds currentPage) {
-		mainFrame.setUrl("help/" + messages.HELP_ROOT() + "/"
-				+ currentPage.getHelpPageValue() + "/index.html");
-		resize(widget);
-	}
+    public void setCurrentWidget(Widget widget, WidgetIds currentPage) {
+        mainFrame.setUrl("help/" + messages.HELP_ROOT() + "/" + currentPage.name() + ".html");
+        resize(widget);
+    }
 
-	public void resize(Widget widget) {
+    public void resize(Widget widget) {
 
-		int width = widget.getOffsetWidth();
-		int height = widget.getOffsetHeight();
+        int width = widget.getOffsetWidth();
+        int height = widget.getOffsetHeight();
 
-		if ((800 - width) < 400) {
-			mainFrame.setWidth("400px");
-		} else {
-			mainFrame.setWidth((800 - width) + "px");
-		}
+        if ((800 - width) < 400) {
+            mainFrame.setWidth("400px");
+        } else {
+            mainFrame.setWidth((800 - width) + "px");
+        }
 
-		if (height < 400) {
-			mainFrame.setHeight("400px");
-		} else {
-			mainFrame.setHeight(height + "px");
-		}
-	}
+        if (height < 400) {
+            mainFrame.setHeight("400px");
+        } else {
+            mainFrame.setHeight(height + "px");
+        }
+    }
 
-	public static HelpPanel getInstance(Elements messages, HelpTexts helpTexts) {
-		if (me == null) {
-			me = new HelpPanel(messages, helpTexts);
-		}
+    public static HelpPanel getInstance(Elements messages, HelpTexts helpTexts) {
+        if (me == null) {
+            me = new HelpPanel(messages, helpTexts);
+        }
 
-		return me;
-	}
+        return me;
+    }
 
-	private HelpPanel(Elements messages, HelpTexts helpTexts) {
-		this.messages = messages;
-		this.helpTexts = helpTexts;
-		disclosurePanel = new DisclosurePanel();
-		disclosurePanel.setHeader(new Label(messages.help()));
-		disclosurePanel.setWidth("100%");
-		disclosurePanel.setHeight("100%");
-		mainFrame = new Frame();
-		contextHelp = new HTML();
-		contextHelp.setStyleName("contexthelp");
+    private HelpPanel(Elements messages, HelpTexts helpTexts) {
+        this.messages = messages;
+        this.helpTexts = helpTexts;
+        disclosurePanel = new DisclosurePanel();
+        disclosurePanel.setHeader(new Label(messages.help()));
+        disclosurePanel.setWidth("100%");
+        disclosurePanel.setHeight("100%");
+        mainFrame = new Frame();
+        contextHelp = new HTML();
+        contextHelp.setStyleName("contexthelp");
 
-		VerticalPanel dp = new VerticalPanel();
-		dp.setWidth("100%");
-		dp.add(contextHelp);
-		dp.add(mainFrame);
-		addEventHandler();
-		disclosurePanel.add(dp);
-		initWidget(disclosurePanel);
+        VerticalPanel dp = new VerticalPanel();
+        dp.setWidth("100%");
+        dp.add(contextHelp);
+        dp.add(mainFrame);
+        addEventHandler();
+        disclosurePanel.add(dp);
+        initWidget(disclosurePanel);
 
-		/* Every second poll and see if help should be updated. */
-		new Timer() {
-			@Override
-			public void run() {
+        /* Every second poll and see if help should be updated. */
+        new Timer() {
+            @Override
+            public void run() {
 
-				if (help != null && prevHelp != help) {
-					contextHelp.setHTML(help);
-					prevHelp = help;
-				}
-			}
+                if (help != null && prevHelp != help) {
+                    contextHelp.setHTML(help);
+                    prevHelp = help;
+                }
+            }
 
-		}.scheduleRepeating(1000);
-	}
+        }.scheduleRepeating(1000);
+    }
 
-	/**
-	 * Each time this is called it makes sure that it is on top of the event
-	 * stack so that it receives events.
-	 */
-	public void addEventHandler() {
-		if (nativePreviewHandler != null) {
-			nativePreviewHandler.removeHandler();
-		}
-		nativePreviewHandler = Event.addNativePreviewHandler(this);
-	}
+    /**
+     * Each time this is called it makes sure that it is on top of the event
+     * stack so that it receives events.
+     */
+    public void addEventHandler() {
+        if (nativePreviewHandler != null) {
+            nativePreviewHandler.removeHandler();
+        }
+        nativePreviewHandler = Event.addNativePreviewHandler(this);
+    }
 
-	public void onPreviewNativeEvent(NativePreviewEvent event) {
-		EventTarget eventTarget = event.getNativeEvent().getEventTarget();
-		
-		if(eventTarget == null) {
-			return;
-		}
-		
-		if(!com.google.gwt.dom.client.Element.is(eventTarget)) {
-			return;
-		}
-		
-		Element elem = Element.as(eventTarget);
-		
-		String id = elem.getPropertyString("id");
+    public void onPreviewNativeEvent(NativePreviewEvent event) {
+        EventTarget eventTarget = event.getNativeEvent().getEventTarget();
 
-		if (id == null) {
-			return;
-		}
+        if (eventTarget == null) {
+            return;
+        }
 
-		String helpText = null;
-		try {
-			helpText = helpTexts.getString(id);
-		} catch (MissingResourceException e) {
-			helpText = "";
-		}
+        if (!com.google.gwt.dom.client.Element.is(eventTarget)) {
+            return;
+        }
 
-		String message = null;
-		try {
-			message = messages.getString(id);
-		} catch (MissingResourceException e) {
-			message = "";
-		}
+        Element elem = Element.as(eventTarget);
 
-		help = formatHelp(message, helpText);
+        String id = elem.getPropertyString("id");
 
-	}
+        if (id == null) {
+            return;
+        }
 
-	private String formatHelp(String header, String text) {
-		return "<strong>" + header + "</strong><br>"
-				+ (text != null ? text : "---");
-	}
+        String helpText = null;
+        try {
+            helpText = helpTexts.getString(id);
+        } catch (MissingResourceException e) {
+            helpText = "";
+        }
+
+        String message = null;
+        try {
+            message = messages.getString(id);
+        } catch (MissingResourceException e) {
+            message = "";
+        }
+
+        help = formatHelp(message, helpText);
+
+    }
+
+    private String formatHelp(String header, String text) {
+        return "<strong>" + header + "</strong><br>" + (text != null ? text : "---");
+    }
 
 }
