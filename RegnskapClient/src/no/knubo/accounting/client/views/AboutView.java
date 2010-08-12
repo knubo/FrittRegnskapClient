b27ad9ec834e2452538c8925c29a01f60ec5805a
knutbo <knutbo@e78aed00-ab2d-0410-9d8d-9fd7b96180e8>
2010-08-12 18:36:11 +0000

    New service for iphone connecting.
    git-svn-id: https://accountclient.googlecode.com/svn/trunk@400 e78aed00-ab2d-0410-9d8d-9fd7b96180e8

--- RegnskapClient/src/no/knubo/accounting/client/views/AboutView.java
+++ RegnskapClient/src/no/knubo/accounting/client/views/AboutView.java
@@ -36,7 +36,7 @@ import com.google.gwt.user.client.ui.Widget;
 public class AboutView extends Composite implements ClickHandler {
 
     /** This must match Version.php's version */
-    public static final String CLIENT_VERSION = "2.05";
+    public static final String CLIENT_VERSION = "2.06";
 
     private static AboutView instance;
 
