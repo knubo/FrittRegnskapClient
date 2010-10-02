371a8eb9d0bd9878cd0a7a22166c3498ae941a27
knutbo <knutbo@e78aed00-ab2d-0410-9d8d-9fd7b96180e8>
2010-10-02 10:41:49 +0000

    Working with Login and remember password.
    git-svn-id: https://accountclient.googlecode.com/svn/trunk@406 e78aed00-ab2d-0410-9d8d-9fd7b96180e8

--- FrittRegnskapMedlemsportal/source/class/frittregnskapmedlemsportal/Login.js
+++ FrittRegnskapMedlemsportal/source/class/frittregnskapmedlemsportal/Login.js
@@ -1,3 +1,4 @@
+
 qx.Class.define("frittregnskapmedlemsportal.Login", {
 	extend : qx.core.Object,
 
@@ -19,18 +20,18 @@ qx.Class.define("frittregnskapmedlemsportal.Login", {
 			return loginNeeded;
 		},
 
-		__effect : null,
-		__container : null,
 		__loginButton : null,
 		__newUserButton: null,
-		__loginName:null,
-		__password:null,
 
 		doLogin : function() {
 			var req = new qx.io.remote.Request("/RegnskapServer/services/portal/portal_authenticate.php?action=login", "POST",
 				"application/json");
-			req.setParameter("user", this.__loginName.getValue(), true);
-			req.setParameter("password", this.__password.getValue() , true);
+			
+            var userField = document.getElementById("userfield");
+            var passwordField = document.getElementById("passwordfield");
+			
+			req.setParameter("user", userField.value, true);
+			req.setParameter("password", passwordField.value , true);
 			
 			var owner = this;
 			
@@ -38,9 +39,9 @@ qx.Class.define("frittregnskapmedlemsportal.Login", {
 				var json = data.getContent();
 
 				if(json["error"]) {
-					owner.__effect.start();
+                    window.alert("Error");
 				} else if(json["result"] == "ok") {
-					window.alert("Login OK");
+                    document.getElementById("loginform").submit();
 				}
 			});
 			
@@ -51,20 +52,22 @@ qx.Class.define("frittregnskapmedlemsportal.Login", {
 		},
 
 		__prepareEffect : function() {
-			this.__effect = new qx.fx.effect.combination.Shake(this.__container.getContainerElement().getDomElement());
+            var userField = document.getElementById("userfield");
 
-			this.__loginName.focus();
+            userField.focus();
 		},
 
 		setupLoginWindow : function(desktop) {
-
+            
+            var htmlElement = document.getElementById("loginbuttons");
+            var inlineIsle = new qx.ui.root.Inline(htmlElement, true, true);
+            inlineIsle.setLayout(new qx.ui.layout.Flow());
+            
+            
 			this.__loginButton = new qx.ui.form.Button("Logg inn");
 			this.__loginButton.setAllowStretchX(false);
 
-			this.__container.add(this.__loginButton, {
-				row : 3,
-				column : 1
-			});
+			inlineIsle.add(this.__loginButton);
 
 			/* Check input on click */
 			this.__loginButton.addListener("execute", this.doLogin, this);
@@ -72,22 +75,19 @@ qx.Class.define("frittregnskapmedlemsportal.Login", {
 			this.__newUserButton = new qx.ui.form.Button("Ny bruker/Glemt passord");
 			this.__newUserButton.setAllowStretchX(false);
 			
-			this.__container.add(this.__newUserButton, {
-				row : 4,
-				column : 1
-			});
+			inlineIsle.add(this.__newUserButton);
 			
 			/* Check input on click */
 			this.__newUserButton.addListener("execute", this.newPassword, this);
 
 		
 			/* Prepare effect as soon as the container is ready */
-			this.__container.addListener("appear", this.__prepareEffect, this);
+			inlineIsle.addListener("appear", this.__prepareEffect, this);
 
 		}
 	},
 	destruct : function() {
-		this._disposeObjects("__container", "__loginButton", "__effect", "__newUserButton", "__loginName", "__password");
+		this._disposeObjects("__loginButton", "__newUserButton");
 	}
 
 });
