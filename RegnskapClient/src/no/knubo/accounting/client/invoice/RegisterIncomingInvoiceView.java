6bd529d1269ccf078d0408fdc01ea3c1a2122667
Knut Erik Borgen <knutbo@ifi.uio.no>
2012-08-12 23:10:12 +0200

    Preparing search for registering invoice

--- RegnskapClient/src/no/knubo/accounting/client/invoice/RegisterIncomingInvoiceView.java
+++ RegnskapClient/src/no/knubo/accounting/client/invoice/RegisterIncomingInvoiceView.java
@@ -6,27 +6,34 @@ import no.knubo.accounting.client.Constants;
 import no.knubo.accounting.client.Elements;
 import no.knubo.accounting.client.I18NAccount;
 import no.knubo.accounting.client.Util;
+import no.knubo.accounting.client.misc.AuthResponder;
+import no.knubo.accounting.client.misc.ServerResponse;
 import no.knubo.accounting.client.ui.AccountTable;
 import no.knubo.accounting.client.ui.DatePickerButton;
+import no.knubo.accounting.client.ui.NamedButton;
 import no.knubo.accounting.client.ui.TextBoxWithErrorText;
 
 import org.gwt.advanced.client.ui.widget.Calendar;
 
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.json.client.JSONValue;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.HorizontalPanel;
 import com.google.gwt.user.client.ui.VerticalPanel;
 
-public class RegisterIncomingInvoiceView extends Composite {
+public class RegisterIncomingInvoiceView extends Composite implements ClickHandler {
 
     private static RegisterIncomingInvoiceView me;
     private final I18NAccount messages;
     private final Constants constants;
     private final Elements elements;
-    private TextBoxWithErrorText invoiceInput;
+    private TextBoxWithErrorText invoiceBox;
     private TextBoxWithErrorText dueDateBox;
     private TextBoxWithErrorText amountBox;
     private TextBoxWithErrorText firstNameBox;
     private TextBoxWithErrorText lastNameBox;
+    private NamedButton searchButton;
 
     public static RegisterIncomingInvoiceView getInstance(I18NAccount messages, Constants constants, Elements elements) {
         if (me == null) {
@@ -43,9 +50,13 @@ public class RegisterIncomingInvoiceView extends Composite {
         VerticalPanel vp = new VerticalPanel();
 
         AccountTable queryTable = new AccountTable("edittable");
+
+        queryTable.setHeadingWithColspan(0, 4, elements.menuitem_invoice_register());
+        queryTable.getRowFormatter().addStyleName(0, "large");
+
         queryTable.setText(1, 0, elements.invoice());
-        invoiceInput = new TextBoxWithErrorText("invoice");
-        queryTable.setWidget(1, 1, invoiceInput);
+        invoiceBox = new TextBoxWithErrorText("invoice");
+        queryTable.setWidget(1, 1, invoiceBox);
 
         dueDateBox = new TextBoxWithErrorText("invoice_due_date_before");
         dueDateBox.setMaxLength(10);
@@ -79,6 +90,13 @@ public class RegisterIncomingInvoiceView extends Composite {
         
         HorizontalPanel buttonPanel = new HorizontalPanel();
         
+        searchButton = new NamedButton("search", elements.search());
+        searchButton.addClickHandler(this);
+        buttonPanel.add(searchButton);
+        
+        queryTable.setWidget(4, 0, buttonPanel);
+        queryTable.getFlexCellFormatter().setColSpan(4, 0, 4);
+        
         
         vp.add(queryTable);
 
@@ -88,6 +106,35 @@ public class RegisterIncomingInvoiceView extends Composite {
         vp.add(resultTable);
 
         initWidget(vp);
+    }
+
+    @Override
+    public void onClick(ClickEvent event) {
+        if(event.getSource() == searchButton) {
+            search();
+        }
+    }
+
+    private void search() {
+        
+        
+        ServerResponse callback = new ServerResponse() {
+            
+            @Override
+            public void serverResponse(JSONValue responseObj) {
+                
+            }
+        };
+        StringBuffer sb = new StringBuffer();
+        sb.append("action=search");
+        
+        Util.addPostParam(sb, "invoice", invoiceBox.getText());
+        Util.addPostParam(sb, "amount", amountBox.getText());
+        Util.addPostParam(sb, "due_date", dueDateBox.getText());
+        Util.addPostParam(sb, "firstname", firstNameBox.getText());
+        Util.addPostParam(sb, "lastname", lastNameBox.getText());
+
+        AuthResponder.post(constants, messages, callback, sb, "accounting/invoice_ops.php");
     }
 
 }
